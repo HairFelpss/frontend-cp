e3587eb92bb63aa89be4a08028ebd3292fa66945
Guilherme <guilhermeanm95@hotmail.com>
2020-06-17 21:24:57 -0300

    Alteraçao de parte do design da pagina de produto

--- src/views/ProductList/data.js
+++ src/views/ProductList/data.js
@@ -3,56 +3,56 @@ import uuid from 'uuid/v1';
 export default [
   {
     id: uuid(),
-    title: 'Dropbox',
+    title: 'Legendary',
     description:
-      'Dropbox is a file hosting service that offers cloud storage, file synchronization, a personal cloud.',
-    imageUrl: '/images/products/product_1.png',
-    totalDownloads: '594',
+      'For those whose fame extends all over the world, making him a legendary adventurer.',
+    imageUrl: '/images/products/treasure_legendary.png',
+    totalDownloads: 'Remaining',
     updatedAt: '27/03/2019'
   },
   {
     id: uuid(),
-    title: 'Medium Corporation',
+    title: 'Épic',
     description:
-      'Medium is an online publishing platform developed by Evan Williams, and launched in August 2012.',
-    imageUrl: '/images/products/product_2.png',
-    totalDownloads: '625',
+      'If you get this, it means you made your name in the world and becomes a epic adventure.',
+    imageUrl: '/images/products/treasure_epic.png',
+    totalDownloads: 'Remaining',
     createdAt: '31/03/2019'
   },
   {
     id: uuid(),
-    title: 'Slack',
+    title: 'Master',
     description:
-      'Slack is a cloud-based set of team collaboration tools and services, founded by Stewart Butterfield.',
-    imageUrl: '/images/products/product_3.png',
-    totalDownloads: '857',
+      'For those who mastered the art of venturing without a problem and made his own fame',
+    imageUrl: '/images/products/treasure_master.jpg',
+    totalDownloads: 'Remaining',
     createdAt: '03/04/2019'
   },
   {
     id: uuid(),
-    title: 'Lyft',
+    title: 'Professional',
     description:
-      'Lyft is an on-demand transportation company based in San Francisco, California.',
-    imageUrl: '/images/products/product_4.png',
-    totalDownloads: '406',
+      'For you who are a Adventure Professional and need a boost .',
+    imageUrl: '/images/products/treasure_professional.png',
+    totalDownloads: 'Remaining',
     createdAt: '04/04/2019'
   },
   {
     id: uuid(),
-    title: 'GitHub',
+    title: 'Intermediate',
     description:
-      'GitHub is a web-based hosting service for version control of code using Git.',
-    imageUrl: '/images/products/product_5.png',
-    totalDownloads: '835',
+      'For adventures who wants start the adventure with some good stuff.',
+    imageUrl: '/images/products/treasure_intermediate.jpg',
+    totalDownloads: 'Remaining',
     createdAt: '04/04/2019'
   },
   {
     id: uuid(),
-    title: 'Squarespace',
+    title: 'Begginer',
     description:
-      'Squarespace provides software as a service for website building and hosting. Headquartered in NYC.',
-    imageUrl: '/images/products/product_6.png',
-    totalDownloads: '835',
+      'The perfect way to start your adventure with some coin.',
+    imageUrl: '/images/products/treasure_begginer.png',
+    totalDownloads: 'Remaining',
     createdAt: '04/04/2019'
   }
 ];
